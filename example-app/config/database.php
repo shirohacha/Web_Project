371d31fcc9a946d396a5c23c16d611ef3ba6e281
shima <shimadak2003@gmail.com>
2023-10-28 01:51:53 +0900

    LalavelとMySQLtt

--- example-app/config/database.php
+++ example-app/config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('http://localhost/phpmyadmin/index.php?route=/database/structure&db=web_project'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'web_project'),
+            'username' => env('DB_USERNAME', 'shima@localhost'),
+            'password' => env('DB_PASSWORD', 'Kakokoki0318'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
